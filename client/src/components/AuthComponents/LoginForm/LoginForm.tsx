a13526fd77e8c5b70d5f1c09a12ec716b6191ea5
JuckieChan <luksor144@gmail.com>
2024-03-05 13:33:40 +0400

    broken proj fixes

--- client/src/components/AuthComponents/LoginForm/LoginForm.tsx
+++ client/src/components/AuthComponents/LoginForm/LoginForm.tsx
@@ -1,12 +1,9 @@
 /* eslint-disable no-nested-ternary */
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
-import type { ChangeEvent} from 'react';
+import type { ChangeEvent } from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import { Button, TextField, Box } from '@mui/material';
 
 type InputsUserType = {
   firstName?: string;
@@ -15,35 +12,34 @@ type InputsUserType = {
   password: string;
   avatar?: string;
   phone?: number;
-}
+};
 
-export default function LoginForm({setIsLogin}): JSX.Element {
-
+export default function LoginForm({ setIsLogin }): JSX.Element {
   const navigate = useNavigate();
 
-  const initialStateRegisterForm = {email: '', password: ''};
+  const initialStateRegisterForm = { email: '', password: '' };
 
-    const [inputs, setInputs] = useState<InputsUserType>(initialStateRegisterForm);
-  
-    const handlerChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
-      setInputs((prev) => ({...prev, [e.target.name]: e.target.value}));
-    }
+  const [inputs, setInputs] = useState<InputsUserType>(initialStateRegisterForm);
 
-    const handlerLogin = async(): Promise<void> => {
-      try {
-        const response = await axios.post(`${import.meta.env.VITE_URL}/user/login`, inputs, {withCredentials: true});
-        if (response.status === 201) {
-          navigate('/');
-          setIsLogin(false);
-          }
-      } catch (error) {
-        console.log(error);
+  const handlerChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handlerLogin = async (): Promise<void> => {
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_URL}/user/login`, inputs, { withCredentials: true });
+      if (response.status === 201) {
+        navigate('/');
+        setIsLogin(false);
       }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-    <div className='authContainer'>
-        <Box
+  return (
+    <div className="authContainer">
+      <Box
         sx={{
           display: 'flex',
           alignItems: 'center',
@@ -54,32 +50,33 @@ export default function LoginForm({setIsLogin}): JSX.Element {
         <TextField
           // helperText="Your email must be unique"
           label="Email"
-          type='text'
-          name='email'
+          type="text"
+          name="email"
           onChange={(e) => void handlerChange(e)}
           sx={{
-              width: '500px',
-              maxWidth: '100%',
-            }}
-            />
+            width: '500px',
+            maxWidth: '100%',
+          }}
+        />
         <TextField
           // helperText="Please enter your password"
-          type='password'
-          name='password'
+          type="password"
+          name="password"
           // id="demo-helper-text-aligned"
           label="Password"
           onChange={(e) => void handlerChange(e)}
           sx={{
-              width: '500px',
-              maxWidth: '100%',
-            }}
-            />
+            width: '500px',
+            maxWidth: '100%',
+          }}
+        />
         <Button variant="contained" color="success" onClick={() => void handlerLogin()}>
-        Войти
+          Войти
         </Button>
-        <Button variant="contained" onClick={() => setIsLogin(false)}>Ещё не зарегистрированы?</Button>
+        <Button variant="contained" onClick={() => setIsLogin(false)}>
+          Ещё не зарегистрированы?
+        </Button>
       </Box>
     </div>
-    )
-  }
-  
+  );
+}
--- client/src/components/AuthComponents/RegisterForm/RegisterForm.tsx
+++ client/src/components/AuthComponents/RegisterForm/RegisterForm.tsx
@@ -1,12 +1,9 @@
 /* eslint-disable no-nested-ternary */
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
-import type { ChangeEvent} from 'react';
+import type { ChangeEvent } from 'react';
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
+import { Button, TextField, Box } from '@mui/material';
 
 type InputsUserType = {
   firstName?: string;
@@ -15,34 +12,33 @@ type InputsUserType = {
   password: string;
   avatar?: string;
   phone?: number;
-}
+};
 
-export default function RegisterForm({setIsLogin}): JSX.Element {
-
+export default function RegisterForm({ setIsLogin }): JSX.Element {
   const navigate = useNavigate();
 
-  const initialStateRegisterForm = {firstName: '', lastName: '',email: '', password: '', avatar: '', phone: 0}
+  const initialStateRegisterForm = { firstName: '', lastName: '', email: '', password: '', avatar: '', phone: 0 };
 
-    const [inputs, setInputs] = useState<InputsUserType>(initialStateRegisterForm);
-  
-    const handlerChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
-      setInputs((prev) => ({...prev, [e.target.name]: e.target.value}));
-    }
+  const [inputs, setInputs] = useState<InputsUserType>(initialStateRegisterForm);
 
-    const handlerRegister = async(): Promise<void> => {
-      try {
-        const response = await axios.post(`${import.meta.env.VITE_URL}/user/register`, inputs, {withCredentials: true})
-        if (response.status === 201) {
-          navigate('/');
-          }
-      } catch (error) {
-        console.log(error);
+  const handlerChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handlerRegister = async (): Promise<void> => {
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_URL}/user/register`, inputs, { withCredentials: true });
+      if (response.status === 201) {
+        navigate('/');
       }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-    <div className='authContainer'>
-        <Box
+  return (
+    <div className="authContainer">
+      <Box
         sx={{
           display: 'flex',
           alignItems: 'center',
@@ -51,49 +47,49 @@ export default function RegisterForm({setIsLogin}): JSX.Element {
         }}
       >
         <TextField
-        label="Ваше имя"
-        type='text'
-        name='firstName'
-        onChange={(e) => void handlerChange(e)}
-            sx={{
-                width: '500px',
-                maxWidth: '100%',
-            }}
-            />
-        <TextField
-        label="Ваша фамилия"
-        type='text'
-        name='lastName'
-        onChange={(e) => void handlerChange(e)}
-            sx={{
-                width: '500px',
-                maxWidth: '100%',
-            }}
-            />
-        <TextField
-        label="Контактный номер телефона"
-        type='text'
-        name='phone'
-        onChange={(e) => void handlerChange(e)}
-            sx={{
-                width: '500px',
-                maxWidth: '100%',
-            }}
-            />
-        <TextField
-          label="Email"
-          helperText="Ваша почта должна быть уникальна"
-          type='text'
-          name='email'
+          label="Ваше имя"
+          type="text"
+          name="firstName"
           onChange={(e) => void handlerChange(e)}
           sx={{
             width: '500px',
             maxWidth: '100%',
           }}
-            />
+        />
         <TextField
-          type='password'
-          name='password'
+          label="Ваша фамилия"
+          type="text"
+          name="lastName"
+          onChange={(e) => void handlerChange(e)}
+          sx={{
+            width: '500px',
+            maxWidth: '100%',
+          }}
+        />
+        <TextField
+          label="Контактный номер телефона"
+          type="text"
+          name="phone"
+          onChange={(e) => void handlerChange(e)}
+          sx={{
+            width: '500px',
+            maxWidth: '100%',
+          }}
+        />
+        <TextField
+          label="Email"
+          helperText="Ваша почта должна быть уникальна"
+          type="text"
+          name="email"
+          onChange={(e) => void handlerChange(e)}
+          sx={{
+            width: '500px',
+            maxWidth: '100%',
+          }}
+        />
+        <TextField
+          type="password"
+          name="password"
           // id="demo-helper-text-aligned"
           label="Пароль"
           onChange={(e) => void handlerChange(e)}
@@ -101,34 +97,35 @@ export default function RegisterForm({setIsLogin}): JSX.Element {
             width: '500px',
             maxWidth: '100%',
           }}
-            />
-          <TextField
+        />
+        <TextField
           label="Загрузить фото"
           helperText="Вы можете загрузить аватар"
-          type='text'
-          name='avatar'
+          type="text"
+          name="avatar"
           onChange={(e) => void handlerChange(e)}
-              sx={{
-                  width: '500px',
-                  maxWidth: '100%',
-              }}
-              />
+          sx={{
+            width: '500px',
+            maxWidth: '100%',
+          }}
+        />
         <TextField
-        helperText="Место забронировано под капчу"
-        type='text'
-        name='lastName'
-        onChange={(e) => void handlerChange(e)}
-            sx={{
-                width: '500px',
-                maxWidth: '100%',
-            }}
-            />
+          helperText="Место забронировано под капчу"
+          type="text"
+          name="lastName"
+          onChange={(e) => void handlerChange(e)}
+          sx={{
+            width: '500px',
+            maxWidth: '100%',
+          }}
+        />
         <Button variant="contained" color="success" onClick={() => void handlerRegister()}>
-        Регистрация
+          Регистрация
         </Button>
-        <Button variant="contained" onClick={() => setIsLogin(true)}>Уже зарегистрированы? Войти</Button>
+        <Button variant="contained" onClick={() => setIsLogin(true)}>
+          Уже зарегистрированы? Войти
+        </Button>
       </Box>
     </div>
-    )
-  }
-  
+  );
+}

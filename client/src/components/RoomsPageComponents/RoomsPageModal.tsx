2ffe5034b96c0090bb67ae8fd9471b3556a6005e
JuckieChan <luksor144@gmail.com>
2024-03-08 13:21:39 +0400

    pet type validation done

--- client/src/components/RoomsPageComponents/RoomsPageModal.tsx
+++ client/src/components/RoomsPageComponents/RoomsPageModal.tsx
@@ -11,14 +11,15 @@ import { RoomType } from '../../types';
 import { fetchCheckOrdersByRoom } from '../../redux/thunkActions';
 import { styled } from '@mui/material/styles';
 import { DateRangePickerDay as MuiDateRangePickerDay, DateRangePickerDayProps } from '@mui/x-date-pickers-pro/DateRangePickerDay';
-import { TextField, Typography, Box, Button, Modal, MenuItem, Select, SelectChangeEvent, InputLabel, FormControl } from '@mui/material';
+import { TextField, Typography, Box, Button, Modal, MenuItem, Select, SelectChangeEvent, InputLabel, FormControl, FormHelperText } from '@mui/material';
+import { useNavigate } from 'react-router';
 
 const style = {
   position: 'absolute' as const,
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: '70%',
+  // width: '70%',
   bgcolor: 'background.paper',
   borderRadius: '4px',
   boxShadow: 24,
@@ -42,37 +43,39 @@ type OrderType = {
   description: string;
 };
 
-const DateRangePickerDay = styled(MuiDateRangePickerDay)(({ theme, isHighlighting, isStartOfHighlighting, isEndOfHighlighting, outsideCurrentMonth }) => ({
-  ...(!outsideCurrentMonth &&
-    isHighlighting && {
-      borderRadius: 0,
-      backgroundColor: theme.palette.primary.main,
-      color: theme.palette.common.white,
-      '&:hover, &:focus': {
-        backgroundColor: theme.palette.primary.dark,
-      },
-    }),
-  ...(isStartOfHighlighting && {
-    borderTopLeftRadius: '50%',
-    borderBottomLeftRadius: '50%',
-  }),
-  ...(isEndOfHighlighting && {
-    borderTopRightRadius: '50%',
-    borderBottomRightRadius: '50%',
-  }),
-})) as React.ComponentType<DateRangePickerDayProps<Dayjs>>;
+// const DateRangePickerDay = styled(MuiDateRangePickerDay)(({ theme, isHighlighting, isStartOfHighlighting, isEndOfHighlighting, outsideCurrentMonth }) => ({
+//   ...(!outsideCurrentMonth &&
+//     isHighlighting && {
+//       borderRadius: 0,
+//       backgroundColor: theme.palette.primary.main,
+//       color: theme.palette.common.white,
+//       '&:hover, &:focus': {
+//         backgroundColor: theme.palette.primary.dark,
+//       },
+//     }),
+//   ...(isStartOfHighlighting && {
+//     borderTopLeftRadius: '50%',
+//     borderBottomLeftRadius: '50%',
+//   }),
+//   ...(isEndOfHighlighting && {
+//     borderTopRightRadius: '50%',
+//     borderBottomRightRadius: '50%',
+//   }),
+// })) as React.ComponentType<DateRangePickerDayProps<Dayjs>>;
 
 export default function RoomsPageModal({ room, open, handleClose }: ModalPropsType) {
   const pets = useAppSelector((store) => store.petSlice.pets);
   const user = useAppSelector((store) => store.userSlice.info);
   const dates = useAppSelector((store) => store.orderSlice.ordersRoom);
   const dispatch = useAppDispatch();
-  const [orders, setOrders] = useState<Array<OrderType>>([]);
+  const navigate = useNavigate();
+  // const [orders, setOrders] = useState<Array<OrderType>>([]);
   const [mainOrder, setMainOrder] = useState<OrderType>({ userId: 0, petId: 0, roomId: 0, dateFrom: '', dateTo: '', amount: 0, quantity: 0, description: '' });
   const [allowPayment, setAllowPayment] = useState(false);
   const [days, setDays] = useState(0);
   const [inputs, setInputs] = useState({ petId: '', allowedRange: '', description: '' });
   const [calendarError, setCalendarError] = useState('');
+  const [petSelectError, setPetSelectError] = useState('');
 
   useEffect(() => {
     if (typeof room?.id === 'number' && room.id !== 0) {
@@ -88,7 +91,6 @@ export default function RoomsPageModal({ room, open, handleClose }: ModalPropsTy
     const checkDateTo = mainOrder.dateTo !== '';
     const checkAmount = mainOrder.amount !== 0;
     const checkQuantity = mainOrder.quantity !== 0;
-
     const userDates = getBannedDates([[mainOrder.dateFrom, mainOrder.dateTo]]);
     const serverDates = getBannedDates(dates);
     if (userDates.flat().some((date) => serverDates.flat().includes(date))) {
@@ -96,17 +98,19 @@ export default function RoomsPageModal({ room, open, handleClose }: ModalPropsTy
     } else {
       setCalendarError('');
     }
-    console.log(checkUserId, checkPetId, checkRoomId, checkDateFrom, checkDateTo, checkAmount, checkQuantity, !calendarError);
-    if (checkUserId && checkPetId && checkRoomId && checkDateFrom && checkDateTo && checkAmount && checkQuantity && !calendarError) {
+    if (checkUserId && checkPetId && checkRoomId && checkDateFrom && checkDateTo && checkAmount && checkQuantity && !calendarError && !petSelectError) {
       setAllowPayment(true);
     } else {
       setAllowPayment(false);
     }
-  }, [mainOrder, calendarError]);
+  }, [mainOrder, calendarError, petSelectError]);
 
   const changeHandler = (event: SelectChangeEvent) => {
     setInputs((prev) => ({ ...prev, [event.target.name]: event.target.value as string }));
-    if (event.target.name === 'petId') setMainOrder((prev) => ({ ...prev, petId: Number(event.target.value) }));
+    if (event.target.name === 'petId') {
+      checkIsPetTypeCorrect(event.target.value);
+      setMainOrder((prev) => ({ ...prev, petId: Number(event.target.value) }));
+    }
     if (event.target.name === 'description') setMainOrder((prev) => ({ ...prev, description: event.target.value }));
   };
 
@@ -132,12 +136,12 @@ export default function RoomsPageModal({ room, open, handleClose }: ModalPropsTy
     }
   };
 
-  const getBannedDates = (dates) => {
+  const getBannedDates = (dates: [string, string][]) => {
     return dates.map((range) => {
       const dateFrom = new Date(range[0]);
       const dateTo = new Date(range[1]);
       const result = [];
-      const pad = (s) => ('00' + s).slice(-2);
+      const pad = (s: number) => ('00' + s).slice(-2);
       while (dateFrom.getTime() <= dateTo.getTime()) {
         result.push(pad(dateFrom.getDate()) + '.' + pad(dateFrom.getMonth() + 1) + '.' + dateFrom.getFullYear());
         dateFrom.setDate(dateFrom.getDate() + 1);
@@ -146,15 +150,42 @@ export default function RoomsPageModal({ room, open, handleClose }: ModalPropsTy
     });
   };
 
+  const checkIsPetTypeCorrect = (choosenPetId: string) => {
+    const choosenPet = pets.find((pet) => pet.id === Number(choosenPetId));
+    if (String(room?.roomPetType).includes(String(choosenPet?.petType))) {
+      console.log('123');
+      setPetSelectError('');
+    } else {
+      setPetSelectError('Выбранный питомец не подходит для этой комнаты');
+      setAllowPayment(false);
+    }
+  };
+
   return (
     <div>
-      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+      <Modal
+        open={open}
+        onClose={() => {
+          handleClose();
+          setInputs({ petId: '', allowedRange: '', description: '' })
+          setPetSelectError('');
+        }}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
         <Box sx={style}>
-          <FormControl sx={{ minWidth: 180 }}>
+          <FormControl fullWidth error={!!petSelectError}>
             <InputLabel id="demo-simple-select-autowidth-label">Выберите вашего питомца</InputLabel>
             <Select labelId="demo-simple-select-autowidth-label" id="demo-simple-select-autowidth" name="petId" value={inputs.petId} onChange={changeHandler} autoWidth label="Выберите вашего питомца">
-              {pets.length && pets.map((pet) => <MenuItem value={String(pet.id)}>{pet.petName}</MenuItem>)}
+              {pets.length ? (
+                pets.map((pet) => <MenuItem value={String(pet.id)}>{pet.petName}</MenuItem>)
+              ) : (
+                <MenuItem>
+                  <Button onClick={() => void navigate('/account/pets/new')}>Создайте карточку питомца!</Button>
+                </MenuItem>
+              )}
             </Select>
+            <FormHelperText>{petSelectError}</FormHelperText>
           </FormControl>
           <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru">
             <DateRangePicker

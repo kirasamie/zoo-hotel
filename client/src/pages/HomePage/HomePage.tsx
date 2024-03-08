d32cbfc928222d1bd15de7a1d08aea4392b069a9
Ekaterina_R <ekaterinaruzakova05@gmail.com>
2024-03-08 16:52:11 +0500

    services

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,18 @@
-import './HomePage.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Pagination } from 'swiper/modules';
-import { YMaps, Map, FullscreenControl, Placemark, TypeSelector, TrafficControl } from '@pbe/react-yandex-maps';
+import "./HomePage.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import {
+  YMaps,
+  Map,
+  FullscreenControl,
+  Placemark,
+  TypeSelector,
+  TrafficControl,
+} from "@pbe/react-yandex-maps";
 
-import 'swiper/css';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/navigation";
+import styles from "./HomePage.module.css";
 
 // const placemark = new Placemark([55.75, 37.61], {
 //   balloonContent: '<img src="http://img-fotki.yandex.ru/get/6114/82599242.2d6/0_88b97_ec425cf5_M" />',
@@ -19,13 +27,16 @@ import 'swiper/css/navigation';
 // geoMap.geoObjects.add(placemark);
 
 export default function HomePage(): JSX.Element {
-  const arrImages = ['first', 'second', 'third', 'fourd'];
+  const arrImages = ["first", "second", "third", "fourd"];
   return (
     <>
       <div className="textContainerLeft"></div>
       <div className="landing__info align__left">
         <h1 className="textContent">ZOoтель</h1>
-        <p className="textContent">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error aut ea fuga cum? Odit, sed culpa harum laborum excepturi quibusdam!</p>
+        <p className="textContent">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error aut ea
+          fuga cum? Odit, sed culpa harum laborum excepturi quibusdam!
+        </p>
         <br />
         <button className="textContent">
           <a href="#/">Забронировать комнату</a>
@@ -58,7 +69,10 @@ export default function HomePage(): JSX.Element {
         </section>
       </div>
       <div className="home-steps-wrapper">
-        <h2>Для того, чтобы начать пользоваться нашим сервисом нужно сделать три простых шага:</h2>
+        <h2>
+          Для того, чтобы начать пользоваться нашим сервисом нужно сделать три
+          простых шага:
+        </h2>
         <div className="home-steps-card-wrapper">
           <div className="home-steps-card">
             <div className="step step-1" />
@@ -83,22 +97,47 @@ export default function HomePage(): JSX.Element {
           </div>
         </div>
       </div>
+      <div className={styles.servicesContainer}>
+        <h2>В стоимость входит:</h2>
+        <div className={styles.services}>
+          <div className={styles.iconText}>
+            <img className={styles.img} src="/img/walkingDog.png" alt="dog" />
+            <div>Выгул 2-3 раза в день</div>
+          </div>
+          <div className={styles.iconText}>
+            <img className={styles.img} src="/img/camera.png" alt="camera" />
+            <div>Фото и видео отчет каждый день</div>
+          </div>
+          <div className={styles.iconText}>
+            <img className={styles.img} src="/img/dogFood.png" alt="dogFood" />
+            <div>Миски и лежанки</div>
+          </div>
+        </div>
+      </div>
+
       <YMaps>
         <div className="ya-map-wrapper">
-          <Map className="ya-map" defaultState={{ center: [46.287837853706414, 47.94919350240244], zoom: 13 }}>
+          <Map
+            className="ya-map"
+            defaultState={{
+              center: [46.287837853706414, 47.94919350240244],
+              zoom: 13,
+            }}
+          >
             <FullscreenControl />
             <Placemark
               geometry={[46.287837853706414, 47.94919350240244]}
-              modules={['geoObject.addon.balloon']}
+              modules={["geoObject.addon.balloon"]}
               properties={{
-                balloonContentHeader: 'Zoотель',
-                balloonContentBody: '<h1 class="ya-map-placemark">ZH</h1><p style="width: 200px">Описание нашего прекрасного отеля которое я не придумал</p>',
-                balloonContentFooter: 'with love by KirKateJuckie',
+                balloonContentHeader: "Zoотель",
+                balloonContentBody:
+                  '<h1 class="ya-map-placemark">ZH</h1><p style="width: 200px">Описание нашего прекрасного отеля которое я не придумал</p>',
+                balloonContentFooter: "with love by KirKateJuckie",
                 autoPan: true,
               }}
             />
-            <TypeSelector options={{ float: 'right' }} />
-            <TrafficControl options={{ float: 'right' }} />
+            <TypeSelector options={{ float: "right" }} />
+            <TrafficControl options={{ float: "right" }} />
           </Map>
         </div>
       </YMaps>

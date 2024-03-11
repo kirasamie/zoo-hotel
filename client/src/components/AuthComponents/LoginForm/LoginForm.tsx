649597c7c6c0e0271d1f6f4ddec0dc3f6339116e
Kirasamie <kirasamie@gmail.com>
2024-03-11 16:31:21 +0300

    validate auth form end

--- client/src/components/AuthComponents/LoginForm/LoginForm.tsx
+++ client/src/components/AuthComponents/LoginForm/LoginForm.tsx
@@ -8,6 +8,15 @@ import { fetchLoginUser } from '../../../redux/thunkActions';
 import { Box, Button, TextField, alpha, styled } from '@mui/material';
 import GlassWrapper from '../../GlassWrapper/GlassWrapper';
 
+function validateEmail(email: string) {
+  return email.toLowerCase().match(/^[a-z/._/0-9]*[@][a-z]*[/.][a-z]{2,4}$/);
+}
+
+export type ErrorMsgType = {
+  email: string;
+  password: string;
+};
+
 const OrangeButton = styled(Button)({
   color: '#ffffff',
   textShadow: '0 1px 2px #000000',
@@ -65,33 +74,94 @@ export default function LoginForm({ setIsLogin }): JSX.Element {
   const navigate = useNavigate();
 
   const initialStateRegisterForm = { email: '', password: '' };
+  const [showError, setShowError] = useState<ErrorMsgType>({});
 
-  const [inputs, setInputs] = useState<InputsUserType>(initialStateRegisterForm);
+  const [inputs, setInputs] = useState<InputsUserType>(
+    initialStateRegisterForm
+  );
 
-  const handlerChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+  const checkError = () => {
+    const errorMsg = {
+      email: '',
+      password: '',
+    };
+    if (inputs.password.trim().length === 0) {
+      errorMsg.password = 'Необходимо ввести ваш пароль!';
+    }
+    if (inputs.email.trim().length === 0) {
+      errorMsg.email = 'Необходимо ввести ваш email!';
+    }
+    if (!validateEmail(inputs.email)) {
+      errorMsg.email =
+        'Ваш email необходимо ввести в формате example@example.com';
+    }
+    setShowError({ ...errorMsg });
+    return Object.entries(errorMsg).some((el) => el[1].length !== 0);
+  };
+
+  const handlerChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   const handlerLogin = async (): Promise<void> => {
-    dispatch(fetchLoginUser(inputs))
-      .then((res) => {
-        if (res.meta.requestStatus === 'fulfilled') {
-          const { isWorker } = res.payload;
-          isWorker ? navigate('/account/orders') : navigate('/account/pets');
-        }
-      })
-      .catch((error) => console.log(error));
+    if (!checkError()) {
+      dispatch(fetchLoginUser(inputs))
+        .then((res) => {
+          if (res.meta.requestStatus === 'fulfilled') {
+            const { isWorker } = res.payload;
+            isWorker ? navigate('/account/orders') : navigate('/account/pets');
+            setShowError((prev) => ({ ...prev, [e.target.name]: '', msg: '' }));
+          } else {
+            const status = Number(res.error.message?.slice(-3));
+            if (status === 401) {
+              setShowError((prev) => ({
+                ...prev,
+                msg: `Почта ${inputs.email} не найдена!`,
+              }));
+            } else if (status === 402) {
+              setShowError((prev) => ({
+                ...prev,
+                msg: `Пароль неверный! Попробуйте ещё раз!`,
+              }));
+            }
+          }
+        })
+        .catch((error) => console.log(error));
+    }
   };
 
   return (
-    <div className="authContainer">
+    <div className='authContainer'>
       <GlassWrapper width='500px'>
-        <OrangeTextfield size='small' variant="outlined" label="Email" type="text" name="email" onChange={(e) => void handlerChange(e)} fullWidth />
-        <OrangeTextfield size='small' variant="outlined" label="Password" type="password" name="password" onChange={(e) => void handlerChange(e)} fullWidth />
-        <OrangeButton variant="outlined" onClick={() => void handlerLogin()}>
+        <div style={{ color: '#d32f2f' }}>{showError.msg}</div>
+        <OrangeTextfield
+          error={!!showError.email}
+          helperText={showError.email}
+          size='small'
+          variant='outlined'
+          label='Email'
+          type='text'
+          name='email'
+          onChange={(e) => void handlerChange(e)}
+          fullWidth
+        />
+        <OrangeTextfield
+          error={!!showError.password}
+          helperText={showError.password}
+          size='small'
+          variant='outlined'
+          label='Password'
+          type='password'
+          name='password'
+          onChange={(e) => void handlerChange(e)}
+          fullWidth
+        />
+        <OrangeButton variant='outlined' onClick={() => void handlerLogin()}>
           Войти
         </OrangeButton>
-        <OrangeButton variant="outlined" onClick={() => setIsLogin(false)}>
+        <OrangeButton variant='outlined' onClick={() => setIsLogin(false)}>
           Ещё не зарегистрированы?
         </OrangeButton>
       </GlassWrapper>

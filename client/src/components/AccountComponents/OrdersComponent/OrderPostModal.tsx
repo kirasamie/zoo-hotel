f49225045b3294e6912a8fd7a90b4261ebf97dac
JuckieChan <luksor144@gmail.com>
2024-03-12 11:23:31 +0400

    orders modal initial

--- client/src/components/AccountComponents/OrdersComponent/OrderPostModal.tsx
+++ client/src/components/AccountComponents/OrdersComponent/OrderPostModal.tsx
@@ -1,30 +1,23 @@
-import { ChangeEvent, useState } from "react";
-import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
-import axios from "axios";
-import { fetchGetPostsByOrder } from "../../../redux/posts/postsThunkActions";
-import { TextField, Box, Button, Modal, styled } from "@mui/material";
+import { ChangeEvent, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
+import axios from 'axios';
+import { fetchGetPostsByOrder } from '../../../redux/posts/postsThunkActions';
+import { Modal, styled } from '@mui/material';
+import StyledTextfield from '../../GlassWrapper/StyledTextfield';
+import StyledButton from '../../GlassWrapper/StyledButton';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import styles from './OrderPostModal.module.css';
+import GlassWrapper from '../../GlassWrapper/GlassWrapper';
 
-const style = {
-  position: "absolute" as const,
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "70%",
-  bgcolor: "background.paper",
-  borderRadius: "4px",
-  boxShadow: 24,
-  p: 4,
-};
-
-const VisuallyHiddenInput = styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
   height: 1,
-  overflow: "hidden",
-  position: "absolute",
+  overflow: 'hidden',
+  position: 'absolute',
   bottom: 0,
   left: 0,
-  whiteSpace: "nowrap",
+  whiteSpace: 'nowrap',
   width: 1,
 });
 
@@ -34,33 +27,25 @@ type ModalPropsType = {
   orderId: string;
 };
 
-export default function OrderPostModal({
-  open,
-  handleClose,
-  orderId,
-}: ModalPropsType) {
+export default function OrderPostModal({ open, handleClose, orderId }: ModalPropsType) {
   const user = useAppSelector((store) => store.userSlice.info);
   const dispatch = useAppDispatch();
   const [inputs, setInputs] = useState({
-    title: "",
-    body: "",
+    title: '',
+    body: '',
     orderId: 0,
     workerId: 0,
   });
-  const [postImage, setPostImage] = useState();
+  const [postImage, setPostImage] = useState<File | null>();
 
   const sendFile = async (postId: number) => {
     if (postImage) {
       const data = new FormData();
-      data.append("postImage", postImage);
-      const response = await axios.post(
-        `${import.meta.env.VITE_URL}/posts/image/${postId}`,
-        data,
-        {
-          headers: { "Content-Type": "multipart/form-data" },
-          withCredentials: true,
-        }
-      );
+      data.append('postImage', postImage);
+      const response = await axios.post(`${import.meta.env.VITE_URL}/posts/image/${postId}`, data, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+        withCredentials: true,
+      });
       if (response.status === 200) {
         dispatch(fetchGetPostsByOrder(Number(orderId)));
         handleClose();
@@ -68,9 +53,7 @@ export default function OrderPostModal({
     }
   };
 
-  const changeHandler = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
+  const changeHandler = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setInputs((prev) => ({
       ...prev,
       [event.target.name]: event.target.value,
@@ -80,64 +63,36 @@ export default function OrderPostModal({
   };
 
   const uploadHandler = async () => {
-    const response = await axios.post(
-      `${import.meta.env.VITE_URL}/posts/`,
-      inputs,
-      { withCredentials: true }
-    );
+    const response = await axios.post(`${import.meta.env.VITE_URL}/posts/`, inputs, { withCredentials: true });
     if (response.status === 200) {
       sendFile(response.data.id);
-      setInputs({ title: "", body: "", orderId: 0, workerId: 0 });
+      setInputs({ title: '', body: '', orderId: 0, workerId: 0 });
     }
   };
 
   return (
-    <Modal
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={style}>
-        <TextField
-          name="title"
-          value={inputs.title}
-          label="Заголовок"
-          fullWidth
-          onChange={changeHandler}
-        />
-        <TextField
-          name="body"
-          value={inputs.body}
-          label="Описание"
-          maxRows={15}
-          multiline
-          fullWidth
-          onChange={changeHandler}
-        />
-
-        <Button
-          component="label"
-          role={undefined}
-          variant="contained"
-          tabIndex={-1}
-          fullWidth
-        >
-          Загрузить фотографию
-          <VisuallyHiddenInput
-            type="file"
-            name="avatar"
-            accept="image/png, image/jpeg, image/jpg"
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              void setPostImage(e.target.files[0])
-            }
-          />
-        </Button>
-
-        <Button variant="contained" onClick={() => void uploadHandler()}>
-          Опубликовать
-        </Button>
-      </Box>
+    <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+      <GlassWrapper width="40vw" className={styles.modalWindow}>
+        <div className={styles.modalForm}>
+          <StyledTextfield name="title" value={inputs.title} label="Заголовок" fullWidth onChange={changeHandler} />
+          <StyledTextfield name="body" value={inputs.body} label="Описание" maxRows={15} multiline fullWidth onChange={changeHandler} />
+          <div className={styles.formWrapper}>
+            <StyledTextfield disabled type="text" name="avatar" value={postImage?.name || 'Файл не выбран'} />
+            <StyledButton sx={{ width: '240px' }} component="label" role={undefined} tabIndex={-1} startIcon={<CloudUploadIcon />} disableElevation>
+              Загрузить фото
+              <VisuallyHiddenInput
+                type="file"
+                name="avatar"
+                accept="image/png, image/jpeg, image/jpg"
+                onChange={(e: ChangeEvent<HTMLInputElement>) => void setPostImage(e.target.files ? e.target.files[0] : null)}
+              />
+            </StyledButton>
+          </div>
+          <StyledButton variant="contained" onClick={() => void uploadHandler()}>
+            Опубликовать
+          </StyledButton>
+        </div>
+      </GlassWrapper>
     </Modal>
   );
 }
